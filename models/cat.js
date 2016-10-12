098d74b86f58a7f37283f69267352859dddab53a
annabialas <ab5086@nyu.edu>
2016-10-12 10:57:22 -0400

    adds more

--- models/cat.js
+++ models/cat.js
@@ -3,14 +3,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var catSchema = new Schema({
-  // name: {type: String, required: true},
-  // slug: {type: String, required: true},
-  // animal: String,
-  // breed: String,
-  // tags: [String],
-  // location: Number,
-  // dateCreated: {type: Date, default: Date.now},
-  // imageURL: String
+  bodega_name: {type: String, required: true},
+  slug: {type: String, required: true},
+  cat_name: String,
+  // tags: not in v.0.1,
+  // location: ?,
+  dateCreated: {type: Date, default: Date.now},
+  imageURL: String
 });
 
 var Cat = mongoose.model('Cat', catSchema);
--- routes/api.js
+++ routes/api.js
@@ -4,3 +4,73 @@ var router = express.Router();
 var Cat = require('../models/cat');
 
 module.exports = router;
+
+
+// FROM CLASS:
+
+// var express = require('express');
+// var router = express.Router();
+
+// var Pet = require('../models/pet');
+
+// router.get('/', function(req, res){
+// 	res.json({
+// 		status: 'ok'
+// 	});
+// });
+
+// router.post('/pet', function(req, res, next){
+// 	var tags = undefined;
+
+// 	if (req.body.tags) {
+// 		tags = req.body.tags.split(',');
+// 	}
+
+// 	var pet = new Pet({
+// 		name: req.body.name,
+// 		slug: req.body.name,
+// 		price: req.body.price,
+//   		breed: req.body.breed,
+//   		tags: tags
+// 	});
+
+// 	pet.save(function(err, data){
+// 		if (err){
+// 			res.status(500);
+// 			console.log(err);
+// 			return res.json({
+// 				status: 'error', 
+// 				message: 'could not create pet',
+// 				error: err
+// 			});
+// 		}
+
+// 		return res.json({
+// 			status: 'ok',
+// 			message: 'created new pet',
+// 			pet: data
+// 		});
+// 	});
+// });
+
+// router.get('/pet', function(req, res, next){
+// 	Pet.find({}, function(err, data){
+// 		if (err){
+// 			res.status(500);
+// 			return res.json({
+// 				status: 'error', 
+// 				message: 'could not get pets'
+// 			});
+// 		}
+
+// 		// if (data.length === 0) {
+// 		// 	return res.json({
+// 		// 		status: 'no pets', 
+// 		// 	});
+// 		// }
+
+// 		return res.json(data);
+// 	});
+// });
+
+// module.exports = router;
